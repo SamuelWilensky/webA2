f5d08bede5d8a228f7b6f02d35a193de693ae274
SamuelWilensky <114035374+SamuelWilensky@users.noreply.github.com>
2025-02-02 18:50:10 -0500

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+// FRONT-END (CLIENT) JAVASCRIPT HERE
+
+const submit = async function( event ) {
+    if(event !== 0) {
+        event.preventDefault()
+    }
+
+    const nameInput = document.querySelector( "#name" ),
+        introInput = document.querySelector( "#intro" ),
+        wingspanInput = document.querySelector( "#wingspan" ),
+        catanInput = document.querySelector( "#catan" ),
+        tickettorideInput = document.querySelector( "#tickettoride" ),
+        clankInput = document.querySelector( "#clank" ),
+        //gamePlayedInput = document.querySelector('input[name="game_played"]:checked'),
+        gameTypeInput = document.querySelector('input[name="game_type"]:checked'),
+        colorInput = document.querySelector( "#colors" );
+
+    let json;
+
+        if(event === 0){
+            json = {name:""}
+        }
+        else {
+            json = {
+                name: nameInput.value,
+                intro: introInput.value,
+                gamePlayed: [wingspanInput.checked, catanInput.checked, tickettorideInput.checked, clankInput.checked],
+                gameType: gameTypeInput.value,
+                playerColor: colorInput.value
+            };
+        }
+        const body = JSON.stringify( json );
+
+    const response = await fetch( "/submit", {
+        method:'POST',
+        body
+    })
+
+    const text = await response.text()
+    const appdata = JSON.parse(text);
+    const resultsTable = document.getElementById("resultsTable");
+
+    resultsTable.innerHTML = "<tr><th>Name</th><th>Intro</th><th>Favorite Mechanic</th><th>Color</th><th>Games Played</th></tr>";
+
+    for (let i = 0; i < appdata.length; i++) {
+        const namei = appdata[i].name;
+        const introi = appdata[i].intro;
+        const typei = appdata[i].gameType;
+        const colori = appdata[i].playerColor;
+        const numi = appdata[i].numGamesPlayed;
+
+        resultsTable.innerHTML += `<tr><th>${namei}</th><th>${introi}</th><th>${typei}</th><th>${colori}</th><th>${numi}</th></tr>`;
+
+    }
+}
+
+window.onload = function() {
+    const button = document.querySelector("button");
+    button.onclick = submit;
+    submit(0);
+}
---
+++ server.js
@@ -0,0 +1,100 @@
+const http = require( "node:http" ),
+    fs   = require( "node:fs" ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library if you're testing this on your local machine.
+    // However, Glitch will install it automatically by looking in your package.json
+    // file.
+    mime = require( "mime" ),
+    dir  = "public/",
+    port = 3000
+
+const appdata = [];
+
+// let fullURL = ""
+const server = http.createServer( function( request,response ) {
+    if( request.method === "GET" ) {
+        handleGet( request, response )
+    }else if( request.method === "POST" ){
+        handlePost( request, response )
+    }
+
+    // The following shows the requests being sent to the server
+    // fullURL = `http://${request.headers.host}${request.url}`
+    // console.log( fullURL );
+})
+
+const handleGet = function( request, response ) {
+    const filename = dir + request.url.slice( 1 )
+
+    if( request.url === "/" ) {
+        sendFile( response, "public/index.html" )
+    }else{
+        sendFile( response, filename )
+    }
+}
+
+const handlePost = function( request, response ) {
+    let dataString = ""
+
+    //if(request.URL === "/submit") {
+
+        request.on("data", function (data) {
+            dataString += data
+        })
+
+
+        request.on("end", function () {
+            const data = JSON.parse(dataString);
+
+            if(data.name !== "") {
+
+                const gamesPlayed = data.gamePlayed;
+                var numGamesPlayed = 0;
+                for (let i = 0; i < gamesPlayed.length; i++) {
+                    if (gamesPlayed[i]) numGamesPlayed++;
+                }
+
+                data.numGamesPlayed = numGamesPlayed;
+
+                console.log(data);
+
+                appdata.push(data);
+            }
+
+            response.writeHead(200, "OK", {"Content-Type": "text/plain"})
+            response.end(JSON.stringify(appdata));
+        })
+
+    /*
+        response.writeHead(200, "OK", {"Content-Type": "text/plain"});
+        console.log(appdata);
+        response.end(JSON.stringify(appdata));
+    */
+}
+
+const sendFile = function( response, filename ) {
+    const type = mime.getType( filename )
+
+    fs.readFile( filename, function( err, content ) {
+
+        // if the error = null, then we've loaded the file successfully
+        if( err === null ) {
+
+            // status code: https://httpstatuses.com
+            response.writeHeader( 200, { "Content-Type": type })
+            response.end( content )
+
+        } else {
+
+            // file not found, error code 404
+            response.writeHeader( 404 )
+            response.end( "404 Error: File Not Found" )
+
+        }
+    })
+}
+
+// process.env.PORT references the port that Glitch uses
+// the following line will either use the Glitch port or one that we provided
+server.listen( process.env.PORT || port )
+
